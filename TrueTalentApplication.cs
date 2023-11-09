2dbbe320c444cd3596533cfbb688389a87232376
Matt Kirshman <mk@metalkindred.com>
2023-11-09 14:38:06 -0800

    fixed calculation so variables represent correct part of theorem

--- TrueTalentApplication.cs
+++ TrueTalentApplication.cs
@@ -1,6 +1,5 @@
 using System;
 using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -18,32 +17,26 @@ public static class TrueTalentApplication
     {
         try
         {
-            // Extract the observed on-base percentage (observedOBP) from the query parameters
+            // Extract the observed on-base probability (P(B|A)) from the query parameters
             if (req.Query.TryGetValue("observedOBP", out var observedOBPValues) && observedOBPValues.Count > 0)
             {
                 if (double.TryParse(observedOBPValues[0], out double observedOBP))
                 {
-                    // Extract the probabilities array from the query parameters
-                    if (req.Query.TryGetValue("probabilities", out var probabilitiesValues) && probabilitiesValues.Count > 0)
+                    // Extract the prior probability (P(B)) from the query parameters
+                    if (req.Query.TryGetValue("probabilityB", out var probabilityBValues) && probabilityBValues.Count > 0)
                     {
-                        // Parse the probabilities array into an array of doubles
-                        var probabilities = probabilitiesValues[0].Split(',').Select(double.Parse).ToArray();
-
-                        // Extract the prior probability (priorProbabilityB) from the query parameters
-                        if (req.Query.TryGetValue("priorProbabilityB", out var priorProbabilityBValues) && priorProbabilityBValues.Count > 0)
+                        if (double.TryParse(probabilityBValues[0], out double probabilityB))
                         {
-                            if (double.TryParse(priorProbabilityBValues[0], out double priorProbabilityB))
+                            // Extract the prior probability (P(A)) from the query parameters
+                            if (req.Query.TryGetValue("probabilityA", out var probabilityAValues) && probabilityAValues.Count > 0)
                             {
-                                // Calculate the probabilityAIfB
-                                double probabilityAIfB = observedOBP;
+                                if (double.TryParse(probabilityAValues[0], out double probabilityA))
+                                {
+                                    // Calculate the conditional probability using Bayes' theorem
+                                    double probabilityAIfB = (observedOBP * probabilityA) / probabilityB;
 
-                                // Calculate the probabilityA
-                                double probabilityA = probabilities.Select(p => p * priorProbabilityB).Sum();
-
-                                // Calculate the conditional probability using Bayes' theorem
-                                double probabilityBIfA = (probabilityAIfB * priorProbabilityB) / probabilityA;
-
-                                return new OkObjectResult(probabilityBIfA);
+                                    return new OkObjectResult(probabilityAIfB);
+                                }
                             }
                         }
                     }
